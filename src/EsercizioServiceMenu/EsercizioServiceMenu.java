9ea8786a93386ccb6027945377ade2bd153f488f
SimoneMurru <15633019+nakedsnake888@users.noreply.github.com>
2020-12-02 15:10:53 +0100

    Added reference to GSON class and HelloWorld!

---
+++ src/EsercizioServiceMenu/EsercizioServiceMenu.java
@@ -0,0 +1,14 @@
+package EsercizioServiceMenu;
+
+import com.google.gson.Gson;
+
+public class EsercizioServiceMenu {
+	
+	public static void main(String[] args) {
+		Gson gson = new Gson();
+		
+		//Parsing di un file JSON per recuperare i dati, da implementare.
+		
+		System.out.println("Hello World!");
+	}
+}
